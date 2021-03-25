6fe65a22778aece939eded6903ef035ab662063d
slash2018 <victor.slash13@hotmail.com>
2021-03-25 00:33:49 -0300

    add bot

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  𝓑𝓞𝓡𝓤𝓝𝓝 𝓑𝓞𝓣
+──────────────────
+    『 *𝑼𝑺𝑼𝑨́𝑹𝑰𝑶 𝑽𝑰𝑷⚜️* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+• *Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* ⚜️🚩`
+}
+exports.cekvip = cekvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 𝐏𝐑𝐎𝐏𝐀𝐆𝐀𝐍𝐃𝐀 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIX E TRANSFERENCIA,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/+556599202754*
+║
+╚═〘  𝓑𝓞𝓡𝓤𝓝𝓝  〙
+`
+}
+exports.iklan = iklan
